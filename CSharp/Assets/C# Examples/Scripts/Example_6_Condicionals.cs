0ac76cab253c709c207ac58cfdd67f32257bec2a
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-07-13 17:03:03 +0200

    End_Section 35

---
+++ CSharp/Assets/C# Examples/Scripts/Example_6_Condicionals.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_6_Condicionals : MonoBehaviour
+{
+    public bool gameOver = false;
+    public bool hasWon = false;
+    public bool hasLost = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if ( gameOver == true) // if (gameOver)
+        {
+
+            Debug.Log("Game Over");
+        }
+        else
+        {
+            Debug.Log("Game is playing.");
+            if (hasWon) Debug.Log("Player Won");
+            else if (hasLost) Debug.Log("Player Lost");
+
+        }
+
+
+
+	}
+}
---
+++ CSharp/Assets/C# Examples/Scripts/Example_7_Multicondicional.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example_7_Multicondicional : MonoBehaviour
+{
+
+    public int lives = 3;
+    public bool canAddLives = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if ( lives > 0 && canAddLives)
+        {
+            Debug.Log("Extra lives");
+        }
+
+        if (lives <= 0 || canAddLives)
+        {
+            Debug.Log("No lives left, but can add Lives");
+        }
+	}
+}
